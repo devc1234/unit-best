a2cdf209d83d9591e29f3981314627b479af0213
devc1234 <namdevchavan713@gmail.com>
2024-06-09 13:05:34 +0530

    gi-1:1546789 THISISMYSECOND

---
+++ 02-Maven-App/src/main/java/in/ashokit/beans/Calculator.java
@@ -0,0 +1,12 @@
+package in.ashokit.beans;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int j = 20;
+
+		System.out.println("Result :: " + (i + j));
+	}
+
+}
